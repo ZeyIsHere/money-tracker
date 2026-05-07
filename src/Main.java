5554080f666df63df9c1ded6e1db43f4c2a15366
ZeyIsHere <rokiwijaya09@gmail.com>
2026-05-07 13:07:05 +0700

    Added interactive expense menu

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,51 @@
 import Model.Expenses;
 import Service.BudgetManager;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        int Choice;
+
         BudgetManager manager = new BudgetManager(100000);
 
-        Expenses food = new Expenses("Food", 25000);
-        Expenses appleMusic = new Expenses("appleMusic", 50000);
+        Scanner input = new Scanner(System.in);
 
-        manager.addExpenses(food);
-        manager.addExpenses(appleMusic);
+        while (true){
+            System.out.println("===MAIN MENU===");
+            System.out.println("1. Add Expense");
+            System.out.println("2. Show Remaining Money");
+            System.out.println("3. Exit");
+            System.out.println("Choose Option: ");
+            int choice = input.nextInt();
+            input.nextLine();
 
-        System.out.println("Remaining Money: " + manager.getRemainingMoney());
+            switch (choice) {
+                case 1:
+                    System.out.print("Add Expense Name: ");
+                    String name = input.nextLine();
+
+                    System.out.print("Add Total Expense: ");
+                    int amount = input.nextInt();
+
+                    Expenses expense = new Expenses(name, amount);
+
+                    manager.addExpenses(expense);
+
+                    System.out.println("Expense Added!");
+
+                    break;
+
+                case 2:
+                    System.out.println(manager.getRemainingMoney());
+
+                    break;
+
+                case 3:
+                    System.out.println("Exiting....");
+
+                    return;
+            }
+        }
     }
 }
